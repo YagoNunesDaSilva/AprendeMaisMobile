1b023036de84cca3850ffe81ac70f4a8f85d60b5
tahkasai <138827369+YagoNunesDaSilva@users.noreply.github.com>
2026-05-25 12:41:42 -0300

    Atualização
    Atualização de telas de cadastro, e tela principal
    Intent nos buttons também foram realizados

--- app/src/main/java/com/example/aprendemais/MainActivity.kt
+++ app/src/main/java/com/example/aprendemais/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.aprendemais
 
-import android.content.Intent // Importação necessária para a Intent
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button // Importação necessária para o Botão
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +12,18 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // Navegação para a Tela Principal
         val btnEntrar = findViewById<Button>(R.id.button)
         btnEntrar.setOnClickListener {
             val intent = Intent(this, TelaPrincipal::class.java)
             startActivity(intent)
-            finish()
+        }
+
+        // Navegação para a Activity de Cadastro
+        val btnCad = findViewById<Button>(R.id.cad)
+        btnCad.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
